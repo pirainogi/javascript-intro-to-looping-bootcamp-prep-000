d142f9e30c60a71b3f86c32189a00085ea47f3ed
Gabbie Piraino <pirainogi@gmail.com>
2018-11-09 02:52:49 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,8 @@
 for (let i = 0; < 25; i++){
   if (i === "1"){
-    console.log("I am" 1 strange loop" + i +" )
+    console.log("I am " + i + "strange loop");
+  }
+  else {
+    console.log("I am " + ${i} + "strange loops")
   }
 }
